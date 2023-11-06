7e71e145abbee3f192a7325bc79b37c1876dfed9
Mykola Manenko <manenkogena847@gmail.com>
2023-11-06 10:20:07 +0200

    the entire code has been optimized

--- parser_ikscs.py
+++ parser_ikscs.py
@@ -9,8 +9,7 @@ import sys
 WORK_TIME_SEC = 12000
 SLEEP_TIME_SEC = 0.2
 LOG_FILE = 'c:\\API\Mykola\ikscs_links\parse.log'
-# BASE = "ikscs.in.ua"
-# BASE = "ingener.in.ua"
+
 
 def format_time(time_duration):
     hours, remainder = divmod(time_duration.seconds, 3600)
@@ -20,37 +19,15 @@ def format_time(time_duration):
 
 def main(BASE, loger):
     
-    
     db = My_base(logger = loger)
 
     if not db.open():
         print('Помилка роботи з базою даних!')
         loger.log('Помилка роботи з базою даних!')
         return 
-    link_for_save_list = []
     
-    sql = f'SELECT `link` FROM parse WHERE `status` = "COMPLETE" AND `domain` = "{BASE}" LIMIT 1'
-    urls = db.get_one_table(sql)
+    start_procedure(db)
     
-    
-    if urls:
-        sql = f'DELETE FROM `parse` WHERE `domain` = "{BASE}"'
-        db.cursor.execute(sql)
-        sql = f'DELETE FROM `parse_img` WHERE `domain` = "{BASE}"'
-        db.cursor.execute(sql)
-        sql = f'INSERT INTO parse (`link`, `domain`) VALUES ("https://{BASE}", "{BASE}")'
-        db.cursor.execute(sql)
-        db.mydb.commit()
-    
-    sql = f'SELECT EXISTS (SELECT 1 FROM `parse` WHERE `domain` = "{BASE}")'
-    is_table_has_data = db.get_one_table(sql)
-    
-    
-    if not is_table_has_data[0]:
-        sql = f'INSERT INTO parse (`link`, `domain`) VALUES ("https://{BASE}", "{BASE}")'
-        db.cursor.execute(sql)
-        db.mydb.commit()
-
 
     count = 0 
     urls = [7]
@@ -64,65 +41,50 @@ def main(BASE, loger):
         urls = db.get_one_table(sql)                   
     
         for url in urls:
-            loger.log(f'...{url}')
-            link_for_save_set, data_uniq, status_code, tags, h_list, a_text_list, link_for_save_list, external_links  = process_one_page(url)
+            uniq_data, status_code, tags, h_list, a_text_list, link_for_save_list, external_links  = process_one_page(url)
             
 
         
-            if link_for_save_set:
-                sql = f'INSERT IGNORE INTO parse (link, status, referer, domain) VALUES (%s, %s, "{url}", "{BASE}")'
-                values = [(e, None) for e in link_for_save_set]
+            if link_for_save_list:
+                sql = f'INSERT IGNORE INTO parse (link, referer, domain) VALUES (%s, %s, %s)'
+                values = []
+                for link in set(link_for_save_list):
+                    has_stop_words = False
+                    for word in ('/shop-basket/add?', '/sklad-cart/add?', '?start=', '/product/view/', '/manufacturer/view/', '/category/view/'):
+                        has_stop_words = has_stop_words or (word in link)
+                    if not has_stop_words:
+                        values.append((link, url, BASE))
                 db.executemany(sql, values)
-                # db.cursor.executemany(sql, values)
 
                 sql = f'INSERT IGNORE INTO parse_a (domain, src, href, anchor) VALUES (%s, %s, %s, %s)'
                 values = []
                 for link, a_text in zip(link_for_save_list, a_text_list):
                     has_stop_words = False
-                    for word in ('/store/', '/manufacturers/manufacturer/', '/sklad-cart/'):
+                    for word in ('/shop-basket/', '/store/', '/manufacturers/manufacturer/', '/sklad-cart/'):
                         has_stop_words = has_stop_words or (word in link)
                     if not has_stop_words:   
                         values.append((BASE, url, link, a_text))
                 db.cursor.executemany(sql, values)
-                # db.mydb.commit()
 
             if external_links:
                 sql = f'INSERT IGNORE INTO parse (domain, link) VALUES (%s, %s)'
-                list_two_save = []
-                for external_link in external_links:
-                    has_stop_words = False
-                    for word in ('/sklad-cart/add?', '?start=', '/store/product/view/', '/manufacturers/manufacturer/view/', '/store/category/view/'):
-                        has_stop_words = has_stop_words or (word in external_link)
-                    if not has_stop_words:
-                        list_two_save.append(external_link)
-                values = [(BASE, external_link) for external_link in list_two_save]
+                values = [(BASE, external_link) for external_link in external_links]
                 db.cursor.executemany(sql, values)
                 db.mydb.commit()
 
-#ancros = link.text
+
             
             sql = 'UPDATE parse SET status="READY", status_code=%s, title=%s, description=%s, lang=%s WHERE link = %s'
             db.cursor.execute(sql, (status_code, tags['title'], tags['description'], tags['lang'], url))
             db.mydb.commit()
             
             sql = f'INSERT IGNORE INTO parse_h (domain, src, level, anchor) VALUES ("{BASE}", "{url}", %s, %s)'
-            #values = [(row[0], row[1]) for row in processed_data]
             db.cursor.executemany(sql, h_list)
             db.mydb.commit()
 
-
-            # sql = f'INSERT IGNORE INTO parse_img (src, title, alt, a_href, refer, domain) VALUES (%s, %s, %s, %s, %s, "{BASE}")'
             
             sql = f'INSERT IGNORE INTO parse_img (src, title, alt, domain) VALUES (%s, %s, %s, "{BASE}")'
-            # data = []
-            values = data_uniq.keys()
-            # for k, v in data_uniq.items():
-            #     data.append([*k, v])
-            # values = []
-            # for row in data:
-            #     values.append((row[0], row[1], row[2]))
-                  # values.append((*row, url))
-            db.cursor.executemany(sql, values)               
+            db.cursor.executemany(sql, uniq_data)               
             db.mydb.commit()
             if datetime.now() - start_time > timedelta(seconds = WORK_TIME_SEC):
                 break
@@ -136,13 +98,11 @@ def main(BASE, loger):
 
 
     
-
 def process_one_page(url):
-    # url = 'https://ingener.in.ua'
     tags = {'title': None, 'description': None, 'lang': None}
 
     if not (url.startswith('http://'+BASE) or url.startswith('https://'+BASE)):
-        return set(), dict(), 0, tags, [], [], [], set()
+        return set(), 0, tags, [], [], [], set()
 
     headers = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
@@ -154,12 +114,10 @@ def process_one_page(url):
     
     status_code = responce.status_code
     if not contentType.startswith('text/html;'):
-        return set(), dict(), status_code, tags, [], [], [], set()
+        return set(), status_code, tags, [], [], [], set()
 
     page = responce.text
-    
     soup = BeautifulSoup(page, "html.parser")
-    # soup = BeautifulSoup(page, "lxml")
 
     # Збираємо всі посилання 
     a_text_list = []
@@ -169,15 +127,13 @@ def process_one_page(url):
         a_text = link.get_text(strip=True)
         a_text_list.append(a_text)
 
-
-    headings = soup.find_all(["h1", "h2", "h3"])
+    # Збираємо заголовки
     h_list = []
-
+    headings = soup.find_all(["h1", "h2", "h3"])
     for heading in headings:
         tag_name = heading.name
         level = None
         text = heading.get_text()
-
         if tag_name and tag_name.startswith("h") and tag_name[1:].isdigit():
             level = int(tag_name[1:])
         else:
@@ -196,29 +152,21 @@ def process_one_page(url):
     html_element = soup.find("html")
     tags['lang'] = html_element.get("lang") if html_element else None
 
+    # Збираємо посилання на картинки
+    uniq_data = set()
     links = soup.find_all('img')
     print(len(links), url)
     loger.log(f'{len(links), url}')
-
-    uniq_data = dict()
-    data = []
     for link in links:
-        row = dict()
         src = link.get('src', link.get('data-src'))
         alt = link.get('alt')
         title = link.get('title')
-        row['src'] = src
-        row['alt'] = alt
-        row['title'] = title                                        
-        data.append(row)
-        uniq_data[(src, title, alt)] = None
+        uniq_data.add((src, title, alt))
         
         
-
-    links = soup.find_all('a', href = True)
-    link_for_save_set = set()
     link_for_save_list = []
     external_links = set()
+    links = soup.find_all('a', href = True)
     for link in links:
         l = link['href']
         link2 = soup.find('img')
@@ -228,26 +176,44 @@ def process_one_page(url):
             src = link2.get('src', link2.get('data-src'))
             alt = link2.get('alt')
             title = link2.get('title')
-            uniq_data[(src, title, alt)] = l
         print(l)
         if l.startswith('tel:') or l.startswith('#') or l.startswith('mailto:'):
             continue
-        if not (l.startswith('/') or l.startswith('.')):# or l.startswith('http://'+BASE) or l.startswith('https://'+BASE)):
+        if not (l.startswith('/') or l.startswith('.')):
             continue
-        #if not (l.startswith('http://'+BASE) or l.startswith('https://'+BASE)):
         if not (l.startswith('http://') or l.startswith('https://')):
             l = 'https://'+ BASE + l
         l = l.split('#')[0]
-        link_for_save_set.add(l)
         link_for_save_list.append(l)
         
-    return link_for_save_set, uniq_data, status_code, tags, h_list, a_text_list, link_for_save_list, external_links
+    return uniq_data, status_code, tags, h_list, a_text_list, link_for_save_list, external_links
 
+def start_procedure(db):
+    sql = f'SELECT `link` FROM parse WHERE `status` = "COMPLETE" AND `domain` = "{BASE}" LIMIT 1'
+    urls = db.get_one_table(sql)
+    
+    if urls:
+        sql = f'DELETE FROM `parse` WHERE `domain` = "{BASE}"'
+        db.cursor.execute(sql)
+        sql = f'DELETE FROM `parse_img` WHERE `domain` = "{BASE}"'
+        db.cursor.execute(sql)
+        sql = f'INSERT INTO parse (`link`, `domain`) VALUES ("https://{BASE}", "{BASE}")'
+        db.cursor.execute(sql)
+        db.mydb.commit()
+    
+    sql = f'SELECT EXISTS (SELECT 1 FROM `parse` WHERE `domain` = "{BASE}")'
+    is_table_has_data = db.get_one_table(sql)
+    
+    if not is_table_has_data[0]:
+        sql = f'INSERT INTO parse (`link`, `domain`) VALUES ("https://{BASE}", "{BASE}")'
+        db.cursor.execute(sql)
+        db.mydb.commit()
+        
 if __name__ == '__main__':
     start_time = datetime.now()
     if len(sys.argv) == 1:
-        # BASE = 'ingener.in.ua'
-        BASE = 'ikscs.in.ua'
+        BASE = 'ingener.in.ua'
+        # BASE = 'ikscs.in.ua'
     elif len(sys.argv) == 2:
         BASE = sys.argv[1]
     else:
