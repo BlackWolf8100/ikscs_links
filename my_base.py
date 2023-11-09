26b6bac8484640317590435ef162c09b2bee0db3
Mykola Manenko <manenkogena847@gmail.com>
2023-11-09 17:45:33 +0200

    add sqlite

--- my_base.py
+++ my_base.py
@@ -3,11 +3,15 @@ import mysql.connector
 import pandas as pd
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
+import sqlite3
 
 class My_base():
-    def __init__(self, logger = False):
-        with open('c:\\API\Mykola\ikscs_links\credentials_MK.json') as f:
-            self.cfg = json.load(f)
+    def __init__(self, dbfile = None, logger = False):
+        if dbfile == None: 
+            with open('c:\\API\Mykola\ikscs_links\credentials_MK.json') as f:
+                self.cfg = json.load(f)
+        else:
+            self.dbfile = dbfile
         self.logger = logger
         
     def log(self, mesage):
@@ -16,12 +20,16 @@ class My_base():
         self.logger.log(mesage)
 
     def open(self):
-        try:
-            self.mydb = mysql.connector.connect(host=self.cfg['host'], user=self.cfg['user'], password=self.cfg['password'], database=self.cfg['db'])#, autocommit=True)
-            self.cursor = self.mydb.cursor(buffered=True)
-        except Exception as eror:
-            print(eror)
-            return False
+        if self.dbfile == None:
+            try:
+                self.mydb = mysql.connector.connect(host=self.cfg['host'], user=self.cfg['user'], password=self.cfg['password'], database=self.cfg['db'])#, autocommit=True)
+                self.cursor = self.mydb.cursor(buffered=True)
+            except Exception as eror:
+                print(eror)
+                return False
+        else:
+            self.mydb = sqlite3.connect(self.dbfile)
+            self.cursor = self.mydb.cursor()
         return True
     
     def get_one_table(self, sql):
@@ -53,7 +61,8 @@ class My_base():
             
     
     def close(self):
-        self.cursor.reset()
+        if not self.dbfile:
+            self.cursor.reset()
         self.cursor.close()
         self.mydb.close()
         
@@ -66,17 +75,21 @@ class My_base():
 
 
 def main():
-    db = My_base()
+    db = My_base(dbfile = 'test.db')
     db.open()
 
     sql = "SELECT * FROM disks"
-
+    
     db.cursor.execute(sql)
     for e in db.cursor.fetchall():
         print(e)
 
-    x = [{'a':1, 'b':2}, {'a':3, 'b':4}]
-    db.export_pd(x, 'test')
+    sql = f'INSERT OR IGNORE INTO disks (Field1, Field2) VALUES (?, ?)'
+    values = (456, 789)
+    db.cursor.execute(sql, values)
+    db.mydb.commit()
+    # x = [{'a':1, 'b':2}, {'a':3, 'b':4}]
+    # db.mydb.export_pd(x, 'test')
 
     db.close()
 
